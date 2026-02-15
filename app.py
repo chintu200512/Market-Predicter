d44d6f6b253c9944569f7881c25386248da89aa5
narendra <narendra200512@gmail.com>
2026-02-15 14:26:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -560,4 +560,5 @@ def support():
     return render_template('support.html', username=session.get('username'))
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000, debug=True)
+
